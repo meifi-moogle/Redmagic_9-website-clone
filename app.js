15406a68436ab1384ad5c2619190440e5bf3fd86
ploop <106459637+meifi-moogle@users.noreply.github.com>
2024-02-17 16:27:45 +0100

    Add animations for phones swaps and more styles

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const phoneImageTwo = document.querySelector('.phone-2-color-0')
 const colorOptions = document.querySelectorAll('.color-option')
 const optionsBorder = document.querySelectorAll('.border')
 
-
+// Function To Display Different Phones and Effects On Clicks 
 for (let i = 0; i < colorOptions.length; i++) {
     colorOptions[i].addEventListener('click', function() {
 
@@ -25,5 +25,20 @@ for (let i = 0; i < colorOptions.length; i++) {
 
         optionsBorder[i].classList.remove('inactive-border')
         optionsBorder[i].classList.add('active-border')
+
+     
+
+        //  Reliably Play Phone Swaps Animations 
+         if (phoneImage.style.animationName === 'slide-in') {
+            phoneImage.style.animation = "slide-in-alt 1000ms"
+        } else {
+            phoneImage.style.animation = "slide-in 1000ms"
+        }
+
+         if (phoneImageTwo.style.animationName === 'slide-in-2') {
+            phoneImageTwo.style.animation = "slide-in-alt-2 1000ms"
+        } else {
+            phoneImageTwo.style.animation = "slide-in-2 1000ms"
+        }
     })
 }
